17e43741f484697017eddab04705e8b91c808ce5
juanitto-maker <alex_nisto@hotmail.com>
2025-08-31 04:47:55 +0200

    Delete theme.js

--- theme.js
+++
@@ -1,8 +0,0 @@
-/*
-theme.js
-Theme configuration and color constants
-2025-08-31 14:20 UTC | Rev 1 | initial theme system with primary brand color
-*/
-
-// Primary brand color constant for consistent theming across the application
-export const PRIMARY_COLOR = '#2980b9';
